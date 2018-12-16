9e912e036d1d8b6ad5c8ec5bbb2beab448db0b5f
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-12-16 16:54:55 +1000

    fix

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+#!/usr/bin/env python3
 import argparse
 from itertools import count
-
 import gym
 import scipy.optimize
-
 import torch
 from models import *
 from replay_memory import Memory
@@ -14,50 +13,72 @@ from utils import *
 
 torch.utils.backcompat.broadcast_warning.enabled = True
 torch.utils.backcompat.keepdim_warning.enabled = True
-
 torch.set_default_tensor_type('torch.DoubleTensor')
 
-parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
-parser.add_argument('--gamma', type=float, default=0.995, metavar='G',
-                    help='discount factor (default: 0.995)')
-parser.add_argument('--env-name', default="Reacher-v1", metavar='G',
-                    help='name of the environment to run')
-parser.add_argument('--tau', type=float, default=0.97, metavar='G',
-                    help='gae (default: 0.97)')
-parser.add_argument('--l2-reg', type=float, default=1e-3, metavar='G',
-                    help='l2 regularization regression (default: 1e-3)')
-parser.add_argument('--max-kl', type=float, default=1e-2, metavar='G',
-                    help='max kl value (default: 1e-2)')
-parser.add_argument('--damping', type=float, default=1e-1, metavar='G',
-                    help='damping (default: 1e-1)')
-parser.add_argument('--seed', type=int, default=543, metavar='N',
-                    help='random seed (default: 1)')
-parser.add_argument('--batch-size', type=int, default=15000, metavar='N',
-                    help='random seed (default: 1)')
-parser.add_argument('--render', action='store_true',
-                    help='render the environment')
-parser.add_argument('--log-interval', type=int, default=1, metavar='N',
-                    help='interval between training status logs (default: 10)')
-args = parser.parse_args()
+def main():
+    args = parse_arg()
+    torch.manual_seed(args.seed)
 
-env = gym.make(args.env_name)
+    env = gym.make(args.env_name)
+    env.seed(args.seed)
+    num_inputs = env.observation_space.shape[0]
+    num_actions = env.action_space.shape[0]
+    running_state = ZFilter((num_inputs,), clip=5)
+    running_reward = ZFilter((1,), demean=False, clip=10)
 
-num_inputs = env.observation_space.shape[0]
-num_actions = env.action_space.shape[0]
+    policy_net = Policy(num_inputs, num_actions)
+    value_net = Value(num_inputs)
 
-env.seed(args.seed)
-torch.manual_seed(args.seed)
+    for i_episode in count(1):
+        memory = Memory()
 
-policy_net = Policy(num_inputs, num_actions)
-value_net = Value(num_inputs)
+        num_steps = 0
+        reward_batch = 0
+        num_episodes = 0
+        while num_steps < args.batch_size:
+            state = env.reset()
+            state = running_state(state)
 
-def select_action(state):
+            reward_sum = 0
+            for t in range(10000): # Don't infinite loop while learning
+                action = select_action(policy_net, state)
+                action = action.data[0].numpy()
+                next_state, reward, done, _ = env.step(action)
+                reward_sum += reward
+
+                next_state = running_state(next_state)
+
+                mask = 1
+                if done:
+                    mask = 0
+
+                memory.push(state, np.array([action]), mask, next_state, reward)
+
+                if args.render:
+                    env.render()
+                if done:
+                    break
+
+                state = next_state
+            num_steps += (t-1)
+            num_episodes += 1
+            reward_batch += reward_sum
+
+        reward_batch /= num_episodes
+        batch = memory.sample()
+        update_params(policy_net, value_net, batch, args)
+
+        if i_episode % args.log_interval == 0:
+            print('Episode {}\tLast reward: {}\tAverage reward {:.2f}'.format(
+                i_episode, reward_sum, reward_batch))
+
+def select_action(policy_net, state):
     state = torch.from_numpy(state).unsqueeze(0)
     action_mean, _, action_std = policy_net(Variable(state))
     action = torch.normal(action_mean, action_std)
     return action
 
-def update_params(batch):
+def update_params(policy_net, value_net, batch, args):
     rewards = torch.Tensor(batch.reward)
     masks = torch.Tensor(batch.mask)
     actions = torch.Tensor(np.concatenate(batch.action, 0))
@@ -113,7 +134,7 @@ def update_params(batch):
                 action_means, action_log_stds, action_stds = policy_net(Variable(states))
         else:
             action_means, action_log_stds, action_stds = policy_net(Variable(states))
-                
+
         log_prob = normal_log_density(Variable(actions), action_means, action_log_stds, action_stds)
         action_loss = -Variable(advantages) * torch.exp(log_prob - Variable(fixed_log_prob))
         return action_loss.mean()
@@ -130,48 +151,30 @@ def update_params(batch):
 
     trpo_step(policy_net, get_loss, get_kl, args.max_kl, args.damping)
 
-running_state = ZFilter((num_inputs,), clip=5)
-running_reward = ZFilter((1,), demean=False, clip=10)
+def parse_arg():
+    parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
+    parser.add_argument('--gamma', type=float, default=0.995, metavar='G',
+                        help='discount factor (default: 0.995)')
+    parser.add_argument('--env-name', default="Reacher-v2", metavar='G',
+                        help='name of the environment to run')
+    parser.add_argument('--tau', type=float, default=0.97, metavar='G',
+                        help='gae (default: 0.97)')
+    parser.add_argument('--l2-reg', type=float, default=1e-3, metavar='G',
+                        help='l2 regularization regression (default: 1e-3)')
+    parser.add_argument('--max-kl', type=float, default=1e-2, metavar='G',
+                        help='max kl value (default: 1e-2)')
+    parser.add_argument('--damping', type=float, default=1e-1, metavar='G',
+                        help='damping (default: 1e-1)')
+    parser.add_argument('--seed', type=int, default=12345, metavar='N',
+                        help='random seed (default: 12345)')
+    parser.add_argument('--batch-size', type=int, default=15000, metavar='N',
+                        help='batch-size (default: 15000)')
+    parser.add_argument('--render', action='store_true',
+                        help='render the environment')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='interval between training status logs (default: 10)')
+    args = parser.parse_args()
+    return args
 
-for i_episode in count(1):
-    memory = Memory()
-
-    num_steps = 0
-    reward_batch = 0
-    num_episodes = 0
-    while num_steps < args.batch_size:
-        state = env.reset()
-        state = running_state(state)
-
-        reward_sum = 0
-        for t in range(10000): # Don't infinite loop while learning
-            action = select_action(state)
-            action = action.data[0].numpy()
-            next_state, reward, done, _ = env.step(action)
-            reward_sum += reward
-
-            next_state = running_state(next_state)
-
-            mask = 1
-            if done:
-                mask = 0
-
-            memory.push(state, np.array([action]), mask, next_state, reward)
-
-            if args.render:
-                env.render()
-            if done:
-                break
-
-            state = next_state
-        num_steps += (t-1)
-        num_episodes += 1
-        reward_batch += reward_sum
-
-    reward_batch /= num_episodes
-    batch = memory.sample()
-    update_params(batch)
-
-    if i_episode % args.log_interval == 0:
-        print('Episode {}\tLast reward: {}\tAverage reward {:.2f}'.format(
-            i_episode, reward_sum, reward_batch))
+if __name__ == '__main__':
+    main()
